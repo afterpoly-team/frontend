75e2024e579169acdfa7e3c1ebd5424ce9ed30ea
evgenii-podruzhinskii <evg.pod.dm@gmail.com>
2021-04-06 00:54:12 +0300

    Add simple pagination without front url support

--- src/components/Content/EventList.jsx
+++ src/components/Content/EventList.jsx
@@ -6,16 +6,13 @@ import { useLanguage } from "../../context/LanguageTranslator";
 import { getLanguage } from "../../utils/getLanguage";
 
 const initialState = {
-  result: {
-    url: "",
-    id: "",
-    title: "",
-    creation_date: "",
-    description: "",
-    event_date: "",
-    link: "",
+  currentUrl: null,
+  fullAPI: {
+    count: 0,
+    next: null,
+    previous: null,
+    results: [],
   },
-  eventsList: [],
 };
 //* const obj = { a: 1, b: 2 };
 
@@ -28,14 +25,34 @@ const EventList = (props) => {
   const [state, setState] = useState(initialState);
   const [loading, setLoading] = useState(false);
 
+  // * if the curUrl from localStore is with wrong language prefix,
+  // * it returns the new correct url
+  const resolveUrls = (eventsUrl, currentUrl) => {
+    let currentUrlPure = currentUrl.substring(0, eventsUrl.length);
+    let result = currentUrl.replace(currentUrlPure, eventsUrl);
+    return result;
+  };
+
   useEffect(async () => {
     try {
       setLoading(true);
       const langUrl = currentLanguage.urlName;
-      const res = await fetch(`http://localhost:8000/${langUrl}/api/events/`);
-      const eventsList = await res.json();
+      const defaultUrl = `http://localhost:8000/${langUrl}/api/events/`;
+      // * url which we will fetch
+      let currentUrl;
 
-      setState((prevState) => ({ ...prevState, eventsList }));
+      // * if the inc/dec buttons were clicked, we resolve new curUrl
+      if (localStorage.getItem("curUrl")) {
+        currentUrl = localStorage.getItem("curUrl");
+        currentUrl = resolveUrls(defaultUrl, currentUrl);
+      } else {
+        currentUrl = defaultUrl;
+      }
+      const res = await fetch(currentUrl);
+      const fullAPI = await res.json();
+
+      console.log("USE EFFECT USED");
+      setState((prevState) => ({ ...prevState, fullAPI, currentUrl }));
       setLoading(false);
     } catch (error) {
       setLoading(false);
@@ -44,8 +61,10 @@ const EventList = (props) => {
   }, []);
 
   const renderEvents = () => {
-    const { eventsList } = state;
+    const eventsList = state.fullAPI.results;
 
+    console.log("STATE:", state);
+    // console.log("LIST:", eventsList);
     return eventsList.map((item) => (
       <li key={item.id}>
         <p>
@@ -63,10 +82,32 @@ const EventList = (props) => {
     ));
   };
 
+  const inc = () => {
+    let currentUrl = state.currentUrl;
+    if (state.fullAPI.next) {
+      currentUrl = state.fullAPI.next;
+    }
+    setState((prevState) => ({ ...prevState, currentUrl }));
+    localStorage.setItem("curUrl", currentUrl);
+    window.location.reload();
+  };
+
+  const dec = () => {
+    let currentUrl = state.currentUrl;
+    if (state.fullAPI.previous) {
+      currentUrl = state.fullAPI.previous;
+    }
+    setState((prevState) => ({ ...prevState, currentUrl }));
+    localStorage.setItem("curUrl", currentUrl);
+    window.location.reload();
+  };
+
   return (
     <main>
       <div>
         <ul>{renderEvents()}</ul>
+        <button onClick={dec}>-1</button>
+        <button onClick={inc}>+1</button>
       </div>
     </main>
   );
